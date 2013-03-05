670cf6ce87fb537750af163c17fa28653bc2d330
Anthony Ringoet <anthonyringoet@gmail.com>
2013-03-05 20:38:02 +0100

    remove unused stuff

--- app.nw/script.js
+++ app.nw/script.js
@@ -1,12 +1,5 @@
 var jade = require('jade');
 
-var data = 'div(class="testing")';
-
-// Compile a function
-var fn = jade.compile(data);
-var html = fn({name:'Oleg'});
-console.log(html);
-
 // dom
 var compileBtn = document.getElementById('compile');
 var input = document.querySelector('.jade-in');
