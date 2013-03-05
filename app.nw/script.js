2ad9f2ee70038ae3413c05cb5746c45570cba854
Anthony Ringoet <anthonyringoet@gmail.com>
2013-03-05 17:50:54 +0100

    initial working version, compiles simple strings

--- app.nw/script.js
+++ app.nw/script.js
@@ -6,3 +6,18 @@ var data = 'div(class="testing")';
 var fn = jade.compile(data);
 var html = fn({name:'Oleg'});
 console.log(html);
+
+// dom
+var compileBtn = document.getElementById('compile');
+var input = document.querySelector('.jade-in');
+var output = document.querySelector('.jade-out');
+
+document.addEventListener('click', function(e){
+  e.preventDefault();
+
+  // make html
+  var fn = jade.compile(input.value);
+  var html = fn();
+
+  output.value = html;
+}, false);
